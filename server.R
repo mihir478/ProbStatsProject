2743cd151f41ffc9552c6018ecadc718c56dbccf
Mihir Sanghavi <mihir@orchardplatform.com>
2016-12-10 18:59:30 -0500

    change symbols

--- server.R
+++ server.R
@@ -2,23 +2,21 @@
 options("getSymbols.warning4.0"=FALSE)
 
 # Makes data request to fetch stock data for a symbol over a year
-makeRequestForYear <- function(symbol, nov_flag, year) {
+makeRequestForYear <- function(symbol, election_flag) {
   data <- NULL
-  if(nov_flag) {
-    from_date <- as.Date(paste("10/08/", year, sep=""), format="%m/%d/%Y")
-    to_date <- as.Date(paste("11/07/", year, sep=""), format="%m/%d/%Y")
-    data <- getSymbols(symbol, auto.assign = FALSE, from = from_date, to = to_date)
-    }  
-  else {
-    from_date <- as.Date(paste("11/08/", year, sep=""), format="%m/%d/%Y")
-    to_date <- as.Date(paste("12/08/", year, sep=""), format="%m/%d/%Y")
+  if(election_flag) {
+    from_date <- as.Date("07/19/2015", format="%m/%d/%Y")
+    data <- getSymbols(symbol, auto.assign = FALSE, from = from_date)
+  } else {
+    from_date <- as.Date("01/01/2016", format="%m/%d/%Y")
+    to_date <- as.Date("07/18/2016", format="%m/%d/%Y")
     data <- getSymbols(symbol, auto.assign = FALSE, from = from_date, to = to_date)
   }
   data
 }
 
 # Caches data to avoid expensive data requests
-getDataForYear <- function(symbol, year, election_flag, envir = parent.frame()) {
+getDataForYear <- function(symbol, election_flag, envir = parent.frame()) {
   data_key <- NULL
   if(election_flag) {
     data_key <- paste(symbol, "Before", sep="")
@@ -26,10 +24,10 @@ getDataForYear <- function(symbol, year, election_flag, envir = parent.frame()) 
     data_key <- paste(symbol, "After", sep="")
   }
   if (is.null(envir[[data_key]])) {
-    envir[[data_key]] <- makeRequestForYear(symbol, election_flag, year)
+    envir[[data_key]] <- makeRequestForYear(symbol, election_flag)
   }
   else {
-    envir[[data_key]] <- getDataForYear(symbol, election_flag, year)
+    envir[[data_key]] <- getDataForYear(symbol, election_flag)
   }
   envir[[data_key]]
 }
@@ -37,9 +35,8 @@ getDataForYear <- function(symbol, year, election_flag, envir = parent.frame()) 
 # Makes data requests and caches responses for a symbol over a year
 require_symbol <- function(symbol, envir = parent.frame()) {
   from_date <- as.Date("01/01/2015", format="%m/%d/%Y")
-  to_date <- as.Date("12/31/2015", format="%m/%d/%Y")
   if (is.null(envir[[symbol]])) {
-    envir[[symbol]] <- getSymbols(symbol, auto.assign = FALSE, from = from_date, to = to_date)
+    envir[[symbol]] <- getSymbols(symbol, auto.assign = FALSE, from = from_date)
   }
   envir[[symbol]]
 }
@@ -49,14 +46,14 @@ shinyServer(function(input, output) {
   symbol_env <- new.env()
   
   # Make a histogram for a symbol to investigate october effect
-  make_hist_elec <- function(symbol, year, election_flag) {
+  make_hist_elec <- function(symbol, election_flag) {
     date_range <- NULL
     if(election_flag) {
-      date_range <- paste("- Date Range: Oct 8 - Nov 7, ", year, sep="")
+      date_range <- "- Date Range: July 19, 2016 - Today"
     } else {
-      date_range <- paste("- Date Range: Nov 8 - Dec 8, ", year, sep="")
+      date_range <- "- Date Range: Jan 1, 2016 - July 18, 2016"
     }
-    symbol_data <- getDataForYear(symbol, year, election_flag, symbol_env)
+    symbol_data <- getDataForYear(symbol, election_flag, symbol_env)
     log_returns = diff(log(symbol_data[,4]))
     log_returns = log_returns[!is.na(log_returns)]
     ggplot(data = log_returns, aes(log_returns)) + 
@@ -65,15 +62,15 @@ shinyServer(function(input, output) {
   }
   
   # Make Q-Q plot
-  make_qqplot <- function(symbol, year) {
+  make_qqplot <- function(symbol) {
     symbol_data <- require_symbol(symbol, symbol_env)
     log_returns = diff(log(symbol_data[,4]))
     log_returns = log_returns[!is.na(log_returns)]
     qqnorm(log_returns, col="#428bca")
   }
   
-  output$qqplot_aapl <- renderPlot({ make_qqplot("AAPL", 2015) })
-  output$qqplot_msft <- renderPlot({ make_qqplot("MSFT", 2015) })
+  output$qqplot_aapl <- renderPlot({ make_qqplot("AAPL") })
+  output$qqplot_xom <- renderPlot({ make_qqplot("XOM") })
   
   #calculate C.I. for mean returns
   get_ci <- function(symbol) {
@@ -83,14 +80,13 @@ shinyServer(function(input, output) {
     sample_size = length(log_returns)
     log_returns_avg = mean(log_returns)
     log_returns_sd = sd(log_returns)
-    #error <- qnorm((1-(input$cilevel/100))/2)*log_returns_sd/sqrt(sample_size)
-    #need to be approximated as t-distribution since population variance is unknown
+    # need to be approximated as t-distribution since population variance is unknown
     error <- qt((1-(input$cilevel/100))/2, sample_size-1)*log_returns_sd/sqrt(sample_size)
     ci <- log_returns_avg + c(-abs(error), abs(error))
     return(ci)
   }
   output$ci_aapl <- renderPrint({get_ci("AAPL")})
-  output$ci_msft <- renderPrint({get_ci("MSFT")})
+  output$ci_xom <- renderPrint({get_ci("XOM")})
   
   # Calculate C.I. for variances
   get_ci_var <- function(symbol) {
@@ -109,7 +105,7 @@ shinyServer(function(input, output) {
   
   # Make C.I. interval for variances
   output$ci_var_aapl <- renderPrint({get_ci_var("AAPL")})
-  output$ci_var_msft <- renderPrint({get_ci_var("MSFT")})
+  output$ci_var_xom <- renderPrint({get_ci_var("XOM")})
   
   # Test the equality of the two population means with unknow population variances
   symbol_env1 <- new.env()
@@ -128,19 +124,19 @@ shinyServer(function(input, output) {
   }
   
   # Output essential t-test results
-  output$aapl_msft_means <- renderTable({
-    mod = test_pop_means("AAPL", "MSFT")
+  output$aapl_xom_means <- renderTable({
+    mod = test_pop_means("AAPL", "XOM")
     tab = matrix(c(mod$parameter,mod$statistic,mod$p.value),nrow=1)
     colnames(tab) = c("Degrees of Freedom","Test-Statistic","P-Value")
     rownames(tab) = "Values"
     tab
   })
   
-  output$october_aapl_return <- renderPlot({ make_hist_elec("AAPL", 2015, TRUE) })
-  output$october_msft_return <- renderPlot({ make_hist_elec("MSFT", 2015, TRUE) })
+  output$october_aapl_return <- renderPlot({ make_hist_elec("AAPL", TRUE) })
+  output$october_xom_return <- renderPlot({ make_hist_elec("XOM", TRUE) })
   
-  output$year_but_october_aapl_return <- renderPlot({ make_hist_elec("AAPL", 2015, FALSE) })
-  output$year_but_october_msft_return <- renderPlot({ make_hist_elec("MSFT", 2015, FALSE) })
+  output$year_but_october_aapl_return <- renderPlot({ make_hist_elec("AAPL", FALSE) })
+  output$year_but_october_xom_return <- renderPlot({ make_hist_elec("XOM", FALSE) })
   
   # Regression of a single stock against time
   reg_onestock <- function(symbol) {
@@ -157,7 +153,7 @@ shinyServer(function(input, output) {
   }
   
   output$reg_onestock_data_plot_aapl <- renderPlot({reg_onestock_data_plot("AAPL")})
-  output$reg_onestock_data_plot_msft <- renderPlot({reg_onestock_data_plot("MSFT")})
+  output$reg_onestock_data_plot_xom <- renderPlot({reg_onestock_data_plot("XOM")})
   
   # One stock residuals plot with least-squares line
   reg_onestock_resid_plot <- function(symbol) {
@@ -167,7 +163,7 @@ shinyServer(function(input, output) {
   }
   
   output$reg_onestock_resid_plot_aapl <- renderPlot({reg_onestock_resid_plot("AAPL")})
-  output$reg_onestock_resid_plot_msft <- renderPlot({reg_onestock_resid_plot("MSFT")})
+  output$reg_onestock_resid_plot_xom <- renderPlot({reg_onestock_resid_plot("XOM")})
   
   # One stock regression intercept and slope estimates
   reg_onestock_coeffs <- function(symbol) {
@@ -176,8 +172,8 @@ shinyServer(function(input, output) {
   output$reg_onestock_coeffs_aapl <- renderDataTable({
     coeffs = reg_onestock_coeffs("AAPL")
   }, options=list(paging = FALSE, searching = FALSE))
-  output$reg_onestock_coeffs_msft <- renderDataTable({
-    coeffs = reg_onestock_coeffs("MSFT")
+  output$reg_onestock_coeffs_xom <- renderDataTable({
+    coeffs = reg_onestock_coeffs("XOM")
   }, options=list(paging = FALSE, searching = FALSE))
   
   # Regression of a two stocks against each other
@@ -202,15 +198,15 @@ shinyServer(function(input, output) {
     abline(reg_twostocks(symbol1, symbol2))
   }
   # Two stocks data with least-squares line
-  output$reg_twostocks_data_plot_aapl_msft <- renderPlot({reg_twostocks_data_plot("AAPL", "MSFT")})
+  output$reg_twostocks_data_plot_aapl_xom <- renderPlot({reg_twostocks_data_plot("AAPL", "XOM")})
   
   # Two stocks regression intercept and slope estimates
   reg_twostocks_coeffs <- function(symbol1, symbol2) {
     summary(reg_twostocks(symbol1, symbol2))$coefficients
   }
   # Two stocks regression intercept and slope estimates
-  output$reg_twostocks_coeffs_aapl_msft <- renderDataTable({
-    coeffs = reg_twostocks_coeffs("AAPL", "MSFT")
+  output$reg_twostocks_coeffs_aapl_xom <- renderDataTable({
+    coeffs = reg_twostocks_coeffs("AAPL", "XOM")
   }, options = list(searching = FALSE, paging = FALSE))
   
   # Two stocks residuals plot
@@ -221,7 +217,7 @@ shinyServer(function(input, output) {
     abline(reg_twostocks(symbol1, symbol2))
   }
   # Two stocks residuals plot
-  output$reg_twostocks_resid_plot_aapl_msft <- renderPlot({reg_twostocks_resid_plot("AAPL", "MSFT")})
+  output$reg_twostocks_resid_plot_aapl_xom <- renderPlot({reg_twostocks_resid_plot("AAPL", "XOM")})
 })
 
 
