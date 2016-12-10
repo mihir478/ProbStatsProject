e907dd07dd20ba6dd1dbf58db828e981cfaa692d
Mihir Sanghavi <mihir@orchardplatform.com>
2016-12-10 12:36:31 -0500

    CI, regression, etc

--- server.R
+++ server.R
@@ -1,11 +1,7 @@
-if (!require(quantmod)) {
-  stop("This app requires the quantmod package. To install it, run 'install.packages(\"quantmod\")'.\n")
-}
-
-# Reduce unnecessary warnings
+# Reduce unnecessary warnings from scaling an xts data structure
 options("getSymbols.warning4.0"=FALSE)
 
-# Download data for a stock if needed, then return that data
+# Makes data request to fetch stock data for a symbol over a year
 makeRequestForYear <- function(symbol, oct_flag, year) {
   data <- NULL
   if(oct_flag) {
@@ -24,6 +20,7 @@ makeRequestForYear <- function(symbol, oct_flag, year) {
   data
 }
 
+# Caches data to avoid expensive data requests
 getDataForYear <- function(symbol, year, oct_flag, envir = parent.frame()) {
   data_key <- NULL
   if(oct_flag) {
@@ -40,9 +37,14 @@ getDataForYear <- function(symbol, year, oct_flag, envir = parent.frame()) {
   envir[[data_key]]
 }
 
-require_symbol <- function(symbol, year, oct_flag, symbol_env) {
-  data <- getDataForYear(symbol, year, oct_flag, symbol_env)
-  data
+# Makes data requests and caches responses for a symbol over a year
+require_symbol <- function(symbol, envir = parent.frame()) {
+  from_date <- as.Date("01/01/2015", format="%m/%d/%Y")
+  to_date <- as.Date("12/31/2015", format="%m/%d/%Y")
+  if (is.null(envir[[symbol]])) {
+    envir[[symbol]] <- getSymbols(symbol, auto.assign = FALSE, from = from_date, to = to_date)
+  }
+  envir[[symbol]]
 }
 
 shinyServer(function(input, output) {
@@ -53,25 +55,200 @@ shinyServer(function(input, output) {
   # Create an environment for storing data
   symbol_env <- new.env()
   
-  # Make a histogram for a symbol, with the settings from the inputs
-  make_hist <- function(symbol, year, oct_flag) {
+  # Make a histogram for a symbol to investigate october effect
+  make_hist_oct <- function(symbol, year, oct_flag) {
     date_range <- NULL
     if(oct_flag) {
-      date_range <- paste("Date Range: Oct, ", year, sep="")
+      date_range <- paste("- Date Range: Oct, ", year, sep="")
     } else {
-      date_range <- paste("Date Range: Jan-Sep and Nov-Dec, ", year, sep="")
+      date_range <- paste("- Date Range: Jan-Sep and Nov-Dec, ", year, sep="")
     }
-    symbol_data <- require_symbol(symbol, year, oct_flag, symbol_env)
+    symbol_data <- getDataForYear(symbol, year, oct_flag, symbol_env)
     log_returns = diff(log(symbol_data[,4]))
     log_returns = log_returns[!is.na(log_returns)]
     ggplot(data = log_returns, aes(log_returns)) + 
-    geom_histogram(binwidth = 1E-2, fill = 'black', color = 'white') +
-    labs(x = "Log Returns", y = "Frequency", title = paste("Log Returns for", symbol, date_range, sep=" "))
+      geom_histogram(binwidth = 1E-2, fill = "#428bca", color = "white") +
+      labs(x = "Log Returns", y = "Frequency", title = paste(symbol, "Log Returns", date_range, sep=" "))
   }
   
-  output$october_aapl_return <- renderPlot({ make_hist("AAPL", 2015, TRUE) })
-  output$october_msft_return <- renderPlot({ make_hist("MSFT", 2015, TRUE) })
+  # Make general histogram for a symbol 
+  make_hist <- function(symbol, year) {
+    symbol_data <- require_symbol(symbol, symbol_env)
+    log_returns = diff(log(symbol_data[,4]))
+    log_returns = log_returns[!is.na(log_returns)]
+    ggplot(data = log_returns, aes(log_returns)) + 
+      geom_histogram(binwidth = 1E-2, fill = "#428bca", color = "white") + 
+      labs(x = "Log Returns", y = "Frequency", title = paste(symbol, " Log Returns - Year: ", year, sep=""))
+  }
   
-  output$year_but_october_aapl_return <- renderPlot({ make_hist("AAPL", 2015, FALSE) })
-  output$year_but_october_msft_return <- renderPlot({ make_hist("MSFT", 2015, FALSE) })
+  #make qqplot
+  make_qqplot <- function(symbol, year) {
+    symbol_data <- require_symbol(symbol, symbol_env)
+    log_returns = diff(log(symbol_data[,4]))
+    log_returns = log_returns[!is.na(log_returns)]
+    qqnorm(log_returns, col="#428bca")
+  }
+  
+  #make histogram
+  output$plot_aapl <- renderPlot({ make_hist("AAPL", 2015) })
+  output$plot_msft <- renderPlot({ make_hist("MSFT", 2015) })
+  
+  #make qqplot
+  output$qqplot_aapl <- renderPlot({ make_qqplot("AAPL", 2015) })
+  output$qqplot_msft <- renderPlot({ make_qqplot("MSFT", 2015) })
+  
+  #calculate C.I. for mean returns
+  get_ci <- function(symbol) {
+    symbol_data <- require_symbol(symbol, symbol_env) 
+    log_returns = diff(log(symbol_data[,4]))
+    log_returns = log_returns[!is.na(log_returns)]
+    sample_size = length(log_returns)
+    log_returns_avg = mean(log_returns)
+    log_returns_sd = sd(log_returns)
+    #error <- qnorm((1-(input$cilevel/100))/2)*log_returns_sd/sqrt(sample_size)
+    #need to be approximated as t-distribution since population variance is unknown
+    error <- qt((1-(input$cilevel/100))/2, sample_size-1)*log_returns_sd/sqrt(sample_size)
+    ci <- log_returns_avg + c(-error, error)
+    return(ci)
+  }
+  #make ci
+  output$ci_aapl <- renderPrint({get_ci("AAPL")})
+  output$ci_msft <- renderPrint({get_ci("MSFT")})
+  
+  #calculate C.I. for variances
+  get_ci_var <- function(symbol) {
+    symbol_data <- require_symbol(symbol, symbol_env) 
+    log_returns = diff(log(symbol_data[,4]))
+    log_returns = log_returns[!is.na(log_returns)]
+    sample_size = length(log_returns)
+    log_returns_avg = mean(log_returns)
+    log_returns_sd = sd(log_returns)
+    
+    left_ci <- ((sample_size-1)*(log_returns_sd^2))/qchisq((1-(input$cilevel/100)/2), sample_size-1)
+    right_ci <- ((sample_size-1)*(log_returns_sd^2))/qchisq(((input$cilevel/100)/2), sample_size-1)
+    ci <- c(left_ci, right_ci)
+    return(ci)
+  }
+  
+  # make ci for variances
+  output$ci_var_aapl <- renderPrint({get_ci_var("AAPL")})
+  output$ci_var_msft <- renderPrint({get_ci_var("MSFT")})
+  
+  # test the equality of the two population means with unknow population variances
+  symbol_env1 <- new.env()
+  symbol_env2 <- new.env()
+  test_pop_means <- function(symbol1, symbol2) {
+    symbol1_data <- require_symbol(symbol1, symbol_env1)
+    symbol2_data <- require_symbol(symbol2, symbol_env2)
+    
+    log_returns1 = diff(log(symbol1_data[,4]))
+    log_returns1 = log_returns1[!is.na(log_returns1)]
+    
+    log_returns2 = diff(log(symbol2_data[,4]))
+    log_returns2 = log_returns2[!is.na(log_returns2)]
+    
+    t.test(scale(log_returns1), scale(log_returns1))
+  }
+  
+  #output essential t-test results
+  output$aapl_msft_means <- renderTable({
+    mod = test_pop_means("AAPL", "MSFT")
+    tab = matrix(c(mod$parameter,mod$statistic,mod$p.value),nrow=1)
+    colnames(tab) = c("Degrees of Freedom","Test-Statistic","P-Value")
+    rownames(tab) = "Values"
+    tab
+  })
+  
+  output$october_aapl_return <- renderPlot({ make_hist_oct("AAPL", 2015, TRUE) })
+  output$october_msft_return <- renderPlot({ make_hist_oct("MSFT", 2015, TRUE) })
+  
+  output$year_but_october_aapl_return <- renderPlot({ make_hist_oct("AAPL", 2015, FALSE) })
+  output$year_but_october_msft_return <- renderPlot({ make_hist_oct("MSFT", 2015, FALSE) })
+  
+  #regression of a single stock against time
+  reg_onestock <- function(symbol) {
+    symbol_data <- require_symbol(symbol, symbol_env) 
+    log_returns = diff(log(symbol_data[,4]))
+    log_returns = log_returns[!is.na(log_returns)]
+    time = seq(1, length(log_returns), 1)
+    lm = lm(formula = log_returns ~ time)
+  }
+  
+  #one stock data with least-squares line
+  reg_onestock_data_plot <- function(symbol) {
+    plot(reg_onestock(symbol)$model[1])
+  }
+  
+  output$reg_onestock_data_plot_aapl <- renderPlot({reg_onestock_data_plot("AAPL")})
+  output$reg_onestock_data_plot_msft <- renderPlot({reg_onestock_data_plot("MSFT")})
+  
+  #one stock residuals plot with least-squares line
+  reg_onestock_resid_plot <- function(symbol) {
+    residuals = resid(reg_onestock(symbol))
+    plot(reg_onestock(symbol)$model$time, residuals, ylab="Residuals", xlab="time", main="Residuals Plot")
+    abline(reg_onestock(symbol))
+  }
+  
+  output$reg_onestock_resid_plot_aapl <- renderPlot({reg_onestock_resid_plot("AAPL")})
+  output$reg_onestock_resid_plot_msft <- renderPlot({reg_onestock_resid_plot("MSFT")})
+
+  
+  #one stock regression intercept and slope estimates
+  reg_onestock_coeffs <- function(symbol) {
+    summary(reg_onestock(symbol))$coefficients
+  }
+  #show the output
+  output$reg_onestock_coeffs_aapl <- renderDataTable({
+    coeffs = reg_onestock_coeffs("AAPL")
+  })
+  output$reg_onestock_coeffs_msft <- renderDataTable({
+    coeffs = reg_onestock_coeffs("MSFT")
+  })
+  
+  
+  #FUNCTION regression of a two stocks against each other
+  symbol_env1 <- new.env()
+  symbol_env2 <- new.env()
+  reg_twostocks <- function(symbol1, symbol2) {
+    symbol1_data <- require_symbol(symbol1, symbol_env1)
+    symbol2_data <- require_symbol(symbol2, symbol_env2)
+    
+    log_returns1 = diff(log(symbol1_data[,4]))
+    log_returns1 = log_returns1[!is.na(log_returns1)]
+    
+    log_returns2 = diff(log(symbol2_data[,4]))
+    log_returns2 = log_returns2[!is.na(log_returns2)]
+    
+    lm = lm(formula = log_returns1 ~ log_returns2)
+  }
+  
+  #FUNCTION two stocks data with least-squares line
+  reg_twostocks_data_plot <- function(symbol1, symbol2) {
+    plot.zoo(reg_twostocks(symbol1, symbol2)$model[2], reg_twostocks(symbol1, symbol2)$model[1], main = "Two Stocks Regression", xlab = "Stock 2", ylab = "Stock 1")
+    abline(reg_twostocks(symbol1, symbol2))
+  }
+  #OUTPUT two stocks data with least-squares line
+  output$reg_twostocks_data_plot_aapl_msft <- renderPlot({reg_twostocks_data_plot("AAPL", "MSFT")})
+  
+  #FUNCTION two stocks regression intercept and slope estimates
+  reg_twostocks_coeffs <- function(symbol1, symbol2) {
+    summary(reg_twostocks(symbol1, symbol2))$coefficients
+  }
+  #OUTPUT two stocks regression intercept and slope estimates
+  output$reg_twostocks_coeffs_aapl_msft <- renderDataTable({
+    coeffs = reg_twostocks_coeffs("AAPL", "MSFT")
+  })
+  
+  #FUNCTION two stocks residuals plot
+  #one stock residuals plot with least-squares line
+  reg_twostocks_resid_plot <- function(symbol1, symbol2) {
+    residuals = resid(reg_twostocks(symbol1, symbol2))
+    plot(residuals, ylab="Residuals", xlab="Time", main="Residuals Plot")
+    abline(reg_twostocks(symbol1, symbol2))
+  }
+  #OUTPUT two stocks residules plot
+  output$reg_twostocks_resid_plot_aapl_msft <- renderPlot({reg_twostocks_resid_plot("AAPL", "MSFT")})
 })
+
+
+
--- ui.R
+++ ui.R
@@ -1,3 +1,8 @@
+if (!require("ggplot2")) 
+  install.packages("ggplot2")
+if(!require("quantmod")) 
+  install.packages("quantmod")
+
 library(shiny)
 library(quantmod)
 library(ggplot2)
@@ -9,13 +14,23 @@ shinyUI(pageWithSidebar(
     wellPanel(
       p(strong("Stock Picker")),
       checkboxInput(inputId = "stock_aapl", label = "Apple (AAPL)",     value = FALSE),
-      checkboxInput(inputId = "stock_msft", label = "Microsoft (MSFT)", value = FALSE)
+      checkboxInput(inputId = "stock_msft", label = "Microsoft (MSFT)", value = FALSE),
+      sliderInput("cilevel", label = h5("Confidence Level %:"), min = 80, max = 99, value = 95, step=1)
     )
   ),
   
   mainPanel(
     tabsetPanel(
-      tabPanel("Log Returns Inspector", 
+      tabPanel("General Log Returns Inspector", 
+               #use multiple conditions to control output
+               conditionalPanel(condition = "input.stock_aapl",
+                                br(),
+                                div(plotOutput(outputId = "plot_aapl"))),
+               
+               conditionalPanel(condition = "input.stock_msft",
+                                br(),
+                                div(plotOutput(outputId = "plot_msft")))),
+      tabPanel("October Effect - Log Returns Inspector", 
                # use multiple conditions to control output
                conditionalPanel(condition = "input.stock_aapl",
                                 br(),
@@ -25,26 +40,55 @@ shinyUI(pageWithSidebar(
                conditionalPanel(condition = "input.stock_msft",
                                 br(),
                                 div(plotOutput(outputId = "october_msft_return")),
-                                div(plotOutput(outputId = "year_but_october_msft_return"))),
-               
-               conditionalPanel(condition = "input.stock_ibm",
+                                div(plotOutput(outputId = "year_but_october_msft_return")))
+      ),
+      tabPanel("Q-Q Plot", 
+               conditionalPanel(condition = "input.stock_aapl",
                                 br(),
-                                div(plotOutput(outputId = "october_ibm_return")),
-                                div(plotOutput(outputId = "year_but_october_ibm_return"))),
-               
-               conditionalPanel(condition = "input.stock_goog",
+                                div(plotOutput(outputId = "qqplot_aapl"))), 
+               conditionalPanel(condition = "input.stock_msft",
                                 br(),
-                                div(plotOutput(outputId = "october_goog_return")),
-                                div(plotOutput(outputId = "year_but_october_goog_return"))),
-               
-               conditionalPanel(condition = "input.stock_yhoo",
+                                div(plotOutput(outputId = "qqplot_msft")))),
+      tabPanel("Confidence Interval for Mean and Variance of Log Returns",
+               conditionalPanel(condition = "input.stock_aapl",
                                 br(),
-                                div(plotOutput(outputId = "october_yhoo_return")),
-                                div(plotOutput(outputId = "year_but_october_yhoo_return")))
-               )
-      # tabPanel("Normal Probability Plot"),
-      # tabPanel("Confidence Interval"),
-      # tabPanel("Regression")
+                                p(strong("Cofidence Interval for Mean of AAPL Returns")),
+                                div(textOutput(outputId = "ci_aapl"))),
+               conditionalPanel(condition = "input.stock_msft",
+                                br(),
+                                p(strong("Cofidence Interval for Mean of MSFT Returns")),
+                                div(textOutput(outputId = "ci_msft"))),
+               conditionalPanel(condition = "input.stock_aapl",
+                                br(),
+                                p(strong("Cofidence Interval for Variances of AAPL Returns")),
+                                div(textOutput(outputId = "ci_var_aapl"))),
+               conditionalPanel(condition = "input.stock_msft",
+                                br(),
+                                p(strong("Cofidence Interval for Variances of MSFT Returns")),
+                                div(textOutput(outputId = "ci_var_msft")))),
+      
+      tabPanel("Two Stocks Means Comparision",
+               conditionalPanel(condition = "input.stock_aapl && input.stock_msft",
+                                br(),
+                                strong("Test output:"),
+                                tableOutput(outputId = "aapl_msft_means"))),
+      tabPanel("Single Stock Returns Regression against Time",
+               conditionalPanel(condition = "input.stock_aapl",
+                                br(),
+                                div(plotOutput(outputId = "reg_onestock_data_plot_aapl")),
+                                div(dataTableOutput(outputId = "reg_onestock_coeffs_aapl")),
+                                div(plotOutput(outputId = "reg_onestock_resid_plot_aapl"))),
+               conditionalPanel(condition = "input.stock_msft",
+                                br(),
+                                div(plotOutput(outputId = "reg_onestock_data_plot_msft")),
+                                div(dataTableOutput(outputId = "reg_onestock_coeffs_msft")),
+                                div(plotOutput(outputId = "reg_onestock_resid_plot_msft")))),
+      tabPanel("Two Stocks Returns Regression against Each Other",
+               conditionalPanel(condition = "input.stock_aapl && input.stock_msft",
+                                br(),
+                                plotOutput(outputId = "reg_twostocks_data_plot_aapl_msft"),
+                                div(dataTableOutput(outputId = "reg_twostocks_coeffs_aapl_msft")),
+                                plotOutput(outputId = "reg_twostocks_resid_plot_aapl_msft")))
     )
   )
 ))
